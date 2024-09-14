3c834bcf0ff5c3840742c82a9a47c4cb14d85742
Eric VanLieshout <ericvanl@buffalo.edu>
2024-09-13 22:45:08 -0500

    update

--- init.lua
+++ init.lua
@@ -1,1 +1,21 @@
 require("config.lazy")
+
+vim.cmd([[
+set number
+]])
+vim.cmd([[
+set wrap
+]])
+vim.cmd([[
+set textwidth=80
+]])
+vim.cmd([[
+set colorcolumn=81,82,91,101
+]])
+--[[
+highlight ColorColumn ctermbg=DarkMagenta
+set tabstop=8
+set shiftwidth=8
+set softtabstop=8
+set smartindent
+]]--
