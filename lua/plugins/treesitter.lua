f42735e7750dcda7525377f5bcdbfd67946ed449
ericvanl-ubugram <ericvanl@buffalo.edu>
2024-12-01 21:14:14 -0600

    treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,10 +4,14 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			--ensure_installed = {"lua", "javascript"},
-			ensure_installed = { "lua" },
+			ensure_installed = {"lua", "javascript"},
+			-- ensure_installed = { "lua" },
 			highlight = {enable = true },
 			indent = {enable = true },
 		})
+		-- On initial set up, each language needs to be manually installed
+		-- via :TSInstall [language name], eg: :TSInstall c<CR> to install c
+		-- parsing; could also add it here, but then it will reinstall each time
+		-- vim.cmd "TSInstall c"
 	end
 }
